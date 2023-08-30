e1aedda6172b11863f9bab7ebdd300f1339e7b1f
Ladraclydebruh03 <116252405+Ladraclydebruh03@users.noreply.github.com>
2023-08-30 16:45:09 +0800

    Update Cs4_Photon_Ladra.java

--- src/cs4_photon_ladra/Cs4_Photon_Ladra.java
+++ src/cs4_photon_ladra/Cs4_Photon_Ladra.java
@@ -4,8 +4,22 @@ public class Cs4_Photon_Ladra {
     public static void main(String[] args) {
         System.out.println("hello po");
         
-        String object= "clyde";
-        System.out.println(object);
+        String object1 = "mouse";
+        String color1 = "black";
+        int price1 = "300";
+
+        String object2 = "phone";
+        String color2 = "gray";
+        int price2 = "10000";
+
+        String object3 = "watch";
+        String color3 = "blue";
+        int price3 = "280";
+
+        System.out.println("Object 1");
+        System.out.println("Object name: " + object1);
+        System.out.println("Color: " + color1);
+        System.out.println("Price: " + price1);
     }
     
 }
